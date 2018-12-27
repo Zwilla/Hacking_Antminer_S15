4501996fa398eed13a66bee8ee138a8c7218d933
Zwilla <info@zwilla.de>
2018-12-27 09:20:51 +0100

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,23 +1,6 @@
 #set -x
 file=/tmp/$$
-if [ -f /etc/bitmain-pub.pem ]; then
-	if [ ! -f fileinfo.sig ]; then
-		echo "RUNME: Cannot Find Signature!!!" >> /tmp/upgrade_result
-		exit 1
-	fi
-	openssl dgst -sha256 -verify /etc/bitmain-pub.pem -signature  fileinfo.sig  fileinfo >/dev/null  2>&1
-	vres=$?
-	if [ $vres -eq 1 ]; then
-		echo "FileList Not Signtured!!!" >> /tmp/upgrade_result
-		exit 2
-	fi	
-	md5sum -s -c fileinfo 
-	vres=$?
-	if [ $vres -eq 1 ]; then
-		echo "FileList Check Failed!!!" >> /tmp/upgrade_result
-		exit 3
-	fi	
-fi
+echo "LMAA!!!" >> /tmp/upgrade_result
 
 if [ -e BOOT.bin ]; then
 	flash_erase /dev/mtd0 0x0 0x40 >/dev/null 2>&1
@@ -35,20 +18,13 @@ if [ -e uImage ]; then
 fi
 
 if [ -e uramdisk.image.gz ]; then
-    md5=`md5sum uramdisk.image.gz | awk {'print $1'}`
-    md5_r=`cat md5_info`
-    if [ $md5 == $md5_r ];then
-		flash_erase /dev/mtd1 0x0 0x100 >/dev/null 2>&1
-		nandwrite -p -s 0x0 /dev/mtd1 uramdisk.image.gz >/dev/null 2>&1
-		if [ -e /dev/mtd4 ]; then
-			flash_erase /dev/mtd4 0x0 0x100 >/dev/null 2>&1
-			nandwrite -p -s 0x0 /dev/mtd4 uramdisk.image.gz >/dev/null 2>&1
-		fi
-	else
-		echo $md5 > /config/md5_error
-		echo $md5_r >> /config/md5_error
-		echo "Error md5! $md5 $md5_r" >> /tmp/upgrade_result
+        flash_erase /dev/mtd1 0x0 0x100 >/dev/null 2>&1
+	nandwrite -p -s 0x0 /dev/mtd1 uramdisk.image.gz >/dev/null 2>&1
+	if [ -e /dev/mtd4 ]; then
+		flash_erase /dev/mtd4 0x0 0x100 >/dev/null 2>&1
+		nandwrite -p -s 0x0 /dev/mtd4 uramdisk.image.gz >/dev/null 2>&1
 	fi
+
 fi
 
 if [ -e /log ]; then
